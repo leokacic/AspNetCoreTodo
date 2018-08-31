09688424ad19bd6427c3ca2a2bb621a7cda14637
Leo <leobrale@yahoo.com>
2018-08-31 13:46:12 +0200

    ponovo ponovo

---
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+        
+    }
+}
